faeba35b2f1ed65dcf2d9c98c6d46573c680f28b
MadhurThareja01 <madhurthareja1105@gmail.com>
2025-05-30 13:06:26 +0530

    feat: completed experiment 3

--- 03-pins/counter.sv
+++ 03-pins/counter.sv
@@ -1,14 +1,20 @@
 module counter (
     input logic clkpulse,       // Clock input
     input logic rst,         // Active low reset
+    input logic up,
+    input logic down,       
     output logic [3:0] led // 4-bit counter output
 );
 
     always @(posedge clkpulse or posedge rst) begin
         if (rst) begin
             led <= 4'b0000; // Reset counter to 0
-        end else begin
+        end else if (up && !down) begin
             led <= led + 1; // Increment counter
+        end else if (!up && down) begin
+            led <= led - 1; // Decrement counter
+        end else begin
+            led <= led; // Hold value
         end
     end
 
